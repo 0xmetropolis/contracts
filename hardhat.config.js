b192f797c6c77bb27b79480b2748cb295b125273
Will Kim <will@sonar-labs.com>
2021-07-22 11:28:50 -0400

    fix: skip ethKeys import on CI/CD

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,30 @@
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
+
 // ethKeys should export an object with private keys as strings
-const ethKeys = require("./ethKeys");
+let ethKeys;
+// This is required so test doesn't fail on CI/CD, because ethKeys isn't pushed up to the repo.
+try {
+  ethKeys = require('./ethKeys');
+} catch {
+  
+}
+
+const networks = {
+  hardhat: {
+    gas: 12000000,
+    blockGasLimit: 0xbebc20,
+    allowUnlimitedContractSize: true,
+    timeout: 1800000000,
+  },
+};
+
+if (ethKeys) {
+  networks.rinkeby = {
+    url: `https://rinkeby.infura.io/v3/69ecf3b10bc24c6a972972666fe950c8`,
+    accounts: [ethKeys.account1],
+  }
+}
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -16,16 +39,5 @@ module.exports = {
     gasPrice: 150,
     coinmarketcap: "89cb5fbd-4c95-4879-a48a-ef63a5939d49",
   },
-  networks: {
-    hardhat: {
-      gas: 12000000,
-      blockGasLimit: 0xbebc20,
-      allowUnlimitedContractSize: true,
-      timeout: 1800000000,
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/69ecf3b10bc24c6a972972666fe950c8`,
-      accounts: [ethKeys.account1],
-    },
-  },
+  networks,
 };
