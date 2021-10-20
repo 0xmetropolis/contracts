3c90d7e4f90e0f7bbf6a06327d1b8412111a65fe
Will Kim <will@sonar-labs.com>
2021-10-20 18:28:15 -0400

    feat: add getDeployments function for multiple deployments

---
+++ test/index.js
@@ -0,0 +1,23 @@
+const { expect } = require("chai");
+const { getDeployment } = require("..");
+const RinkebyController = require("../deployments/rinkeby/Controller.json");
+
+describe("Get Deployment", () => {
+  it("should throw if the network is invalid", () => {
+    expect(() => {
+      getDeployment("Controller", "ropsten");
+    }).to.throw("Invalid network");
+  });
+
+  it("should throw if the contract is invalid", () => {
+    expect(() => getDeployment("wrongcontract", "rinkeby")).to.throw("Invalid contract name");
+  });
+
+  it("should be able to find contracts regardless of casing", () => {
+    expect(getDeployment("ConTroller", "rinkeby")).to.equal(RinkebyController);
+  });
+
+  it("should be able to find contracts using network ID instead of name", () => {
+    expect(getDeployment("ConTroller", 4)).to.equal(RinkebyController);
+  });
+});
