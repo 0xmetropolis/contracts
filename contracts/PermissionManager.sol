076292fd0f714208275de911c7a69f745a506969
Will Kim <will@sonar-labs.com>
2022-10-19 10:57:32 -0400

    fix: QSP-6

--- contracts/PermissionManager.sol
+++ contracts/PermissionManager.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.8.7;
 
 import "openzeppelin-contracts/access/AccessControl.sol";
+import "openzeppelin-contracts/utils/Address.sol";
 
 // This contract will be the owner of all other contracts in the ecosystem.
 contract PermissionManager is AccessControl {
@@ -13,6 +14,7 @@ contract PermissionManager is AccessControl {
         public
         onlyRole(DEFAULT_ADMIN_ROLE)
     {
+        Address.isContract(contractAddress);
         // Call uses the context of this contract, so msg.sender of other contracts
         // will be this contract.
         (bool success, ) = contractAddress.call(data);
